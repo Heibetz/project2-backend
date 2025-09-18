cfa0f3d208b1c88724182c970047b352ca279f95
chloewalker <chloe.walker@eagles.oc.edu>
2025-09-18 17:25:28 -0500

    Add MariaDB connection test script

---
+++ test-db-connection.js
@@ -0,0 +1,21 @@
+const mariadb = require('mariadb');
+const dbConfig = require('./routes/db.config');
+
+async function testConnection() {
+  let conn;
+  try {
+    conn = await mariadb.createConnection({
+      host: dbConfig.HOST,
+      user: dbConfig.USER,
+      password: dbConfig.PASSWORD,
+      database: dbConfig.DB
+    });
+    console.log('Connection successful!');
+  } catch (err) {
+    console.error('Connection failed:', err);
+  } finally {
+    if (conn) await conn.end();
+  }
+}
+
+testConnection();
