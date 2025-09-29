05a145bb09d5ed5d62727252f946acc08f233b0e
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-09-29 11:09:23 -0500

    Update App file and testing connection

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cors = require('cors');
+var http = require('http');
+var db = require('./app/models');
+var cookieParser = require('cookie-parser');
+var indexRouter = require('./app/routes/index');
+var coursesRouter = require('./app/routes/courses');
 
 var app = express();
 
@@ -14,19 +16,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/courses', coursesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -37,5 +39,24 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Start server if run directly (not when imported)
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  (async () => {
+    try {
+      await db.sequelize.authenticate();
+      // Do not force sync; assumes tables already exist
+      await db.sequelize.sync();
+      app.set('port', PORT);
+      http.createServer(app).listen(PORT, () => {
+        console.log(`Server listening on http://localhost:${PORT}`);
+      });
+    } catch (err) {
+      console.error('Failed to start server:', err);
+      process.exit(1);
+    }
+  })();
+}
 
 module.exports = app;
