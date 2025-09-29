833eab198c2224703dee90c16fc3931a98f361f2
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-09-29 11:20:33 -0500

    remove delete all router

--- app/routes/courses.js
+++ app/routes/courses.js
@@ -8,6 +8,6 @@ router.get('/:id', courses.findOne);
 router.post('/', courses.create);
 router.put('/:id', courses.update);
 router.delete('/:id', courses.delete);
-router.delete('/', courses.deleteAll);
+//router.delete('/', courses.deleteAll);
 
 module.exports = router;
