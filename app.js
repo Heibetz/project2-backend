43af76c0528bf8acfdb663ea0d68c5c3938dbadf
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-10-01 18:15:12 -0500

    changed from jade to pug

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(cors());
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/courses-t5/courses', coursesRouter);
+app.use('/course-t5/courses', coursesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
