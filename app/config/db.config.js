746e56efd47164164769eec1024fe8ba16f3b20a
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-09-30 20:20:59 -0500

    Add test file and config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,24 @@
+// module.exports = {
+//     HOST: 'localhost',
+//     USER: 'root',
+//     PASSWORD: '',
+//     DB: 'seiv_project2_t5',
+//     dialect: 'mariadb',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// };
+
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'seiv_project2_t5',
-    dialect: 'mariadb',
+    HOST: 't0-database.czjofbims6cw.us-west-2.rds.amazonaws.com',
+    port: 3306,
+    USER: 'admin',
+    PASSWORD: 'passwordt0',
+    DB: 'course',
+    dialect: 'mysql',
     pool: {
         max: 5,
         min: 0,
