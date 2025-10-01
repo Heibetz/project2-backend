abaafc8d7b81a298610192c1f1e7bc4051d0fdee
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-10-01 17:03:52 -0500

    updated config files for deployment

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/courses', coursesRouter);
+app.use('/courses-t5/courses', coursesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
