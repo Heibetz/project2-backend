9ed35cdcf8390df0b32d3bccecf86f7ed894aa7e
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-10-01 17:53:18 -0500

    hardcode port

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -33,7 +33,7 @@ const _base = module.exports || {};
 module.exports = {
   ..._base,
   HOST: process.env.DB_HOST,
-  port: Number(process.env.PORT),
+  port: 3306,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PW,
   DB: process.env.DB_NAME,
